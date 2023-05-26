d2acef41f7bd71be8ae87dda7588579ce9b32bd3
david onwuli <dcazicruz@icloud.com>
2023-05-26 18:37:42 +0100

    task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -12,7 +12,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
@@ -20,7 +20,7 @@ class RedactingFormatter(logging.Formatter):
         return filter_datum(
             self.fields,
             self.REDACTION,
-            super(RedactingFormatter, self).format(record),
+            self.FORMAT,
             self.SEPARATOR)
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
