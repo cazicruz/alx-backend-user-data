e7d3059c3e16cbcdd71a7509e198d7b98e954f36
david onwuli <dcazicruz@icloud.com>
2023-05-27 06:33:08 +0100

    task0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,6 +13,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """ initializing the class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
