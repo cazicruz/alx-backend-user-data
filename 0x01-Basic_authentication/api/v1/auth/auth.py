82828f83e8517c691771dc5b34bb08778abf631c
david onwuli <dcazicruz@icloud.com>
2023-05-31 07:55:28 +0100

    task0

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,6 +17,12 @@ class Auth():
             return True
         if path in excluded_paths:
             return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
         return True
     
     def authorization_header(self, request=None) -> str:
