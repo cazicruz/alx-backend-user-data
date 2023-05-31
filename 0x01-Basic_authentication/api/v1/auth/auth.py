210d67b5ad957418cd423d63fc4e3847c038692f
David Onwuli <dcazicruz@icloud.com>
2023-05-31 09:40:20 +0100

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,7 +34,6 @@ class Auth():
             return None
         return request.headers.get('Authorization')
     
-
     def current_user(self, request=None) -> str:
         """ current user"""
         return None
