b89d4701abb6f02af6a8883e6905cad9ea680a32
david onwuli <dcazicruz@icloud.com>
2023-05-31 08:12:20 +0100

    task0

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -34,9 +34,9 @@ def before_request():
     if not auth.require_auth(request.path, excluded_paths):
         return
     if auth.authorization_header(request) is None:
-        abort(401)
+        abort(401, description="Unauthorized")
     if auth.current_user(request) is None:
-        abort(403)
+        abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
