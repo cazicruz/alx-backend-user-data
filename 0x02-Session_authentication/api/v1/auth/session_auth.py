8813f9e1cbd10bb8fc7cb9109b1f55be009cf4bb
david onwuli <dcazicruz@icloud.com>
2023-06-01 18:14:09 +0100

    task4

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -30,5 +30,7 @@ class SessionAuth(Auth):
         """
         returns user instance based on cookie value
         """
+        if request is None:
+            return None
         session_id_from_cookie = self.session_cookie(request)
         return self.user_id_for_session_id(session_id_from_cookie)
