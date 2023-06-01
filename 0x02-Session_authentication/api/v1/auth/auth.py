933b670f741f2e3a8da039b1e70fe8864101ddfc
david onwuli <dcazicruz@icloud.com>
2023-06-01 10:51:59 +0100

    task0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+""" auth class"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Require auth
+        """
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths:
+            return False
+        else:
+            for i in excluded_paths:
+                if i.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+        return True
+    
+    def authorization_header(self, request=None) -> str:
+        """Authorization header
+        """
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+    
+    def current_user(self, request=None) -> str:
+        """
+        Returns a User instance from information from a request object
+        """
+        return None
