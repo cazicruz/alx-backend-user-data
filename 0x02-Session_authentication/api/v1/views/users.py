da21841dad7f04933e2c71e1edeb3ac2e4c0d1a0
david onwuli <dcazicruz@icloud.com>
2023-06-01 11:44:29 +0100

    task0

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -32,7 +32,8 @@ def view_one_user(user_id: str = None) -> str:
         abort(404)
     if user_id is me and request.current_user is None:
         abort(404)
-    return jsonify(user.to_json())
+    else:
+        return jsonify(user.to_json())
 
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
