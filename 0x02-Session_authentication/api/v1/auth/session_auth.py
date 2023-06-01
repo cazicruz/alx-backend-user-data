8db740fde136f357706b59d85f5b15258ea65317
david onwuli <dcazicruz@icloud.com>
2023-06-01 17:15:09 +0100

    task0

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -17,6 +17,7 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """retrieves the value (user id) stored in 
         user_id_by_session_id with the key (session id)
@@ -24,4 +25,4 @@ class SessionAuth(Auth):
         """
         if session_id is None or type(session_id) != str:
             return None
-        return user_id_by_session_id.get(session_id)
+        return self.user_id_by_session_id.get(session_id)
