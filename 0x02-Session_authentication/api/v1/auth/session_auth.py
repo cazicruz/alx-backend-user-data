0bb24a02b78fe096693dcff254e8890dfa72696e
david onwuli <dcazicruz@icloud.com>
2023-06-01 15:28:44 +0100

    task0

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,15 @@
 """ session_authentication"""
 from .auth import Auth
 import os
+import uuid
 
 
 class SessionAuth(Auth):
     """ implementation of session authentication"""
-    pass
+    user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or type(user_id) != str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
