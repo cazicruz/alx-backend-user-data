28ab95ea0d45141340d61c713634ca68ff184fc4
david onwuli <dcazicruz@icloud.com>
2023-06-07 15:57:34 +0100

    made a Users model with table named users

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,8 @@
+<h1>0x03-user_authentication_service</h1>
+<h2>
+In the industry, you should not implement your own authentication system and use a module or framework that doing it for you (like in Python-Flask: <strong>Flask-User</strong>). Here, for the learning purpose, we will walk through each step of this mechanism to understand it by doing it your self.
+</h2>
+<h3>
+here we will create a user model and assign values to it.
+create a user model class method to impliment add_user method
+and from ther we would continue to improve on this method</h3>
---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""SQLAlchemy model named User for a database table named users (by using the mapping declaration of SQLAlchemy)."""
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+app = Flask(__name__)
+app.config['SECFRET_KEY'] = 'hardtoguess'
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(bassedir, 'database.db')
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.column(db.Integer, primary_key=True)
+    email = db.column(db.String(120), nullable=False)
+    hashed_password = db.column(db.String(240), nullable=False)
+    session_id = db.column(db.String(120, nullable=True)
+    reset_token =  db.column(db.String(120), nullable=True)
