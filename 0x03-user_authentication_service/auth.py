cb5ec2809d10f5cf53c38f9bb7f2cbe469c933ff
david onwuli <dcazicruz@icloud.com>
2023-06-07 18:07:53 +0100

    changed the __session to _session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -6,6 +6,14 @@ import bcrypt
 
 
 def _hash_password(password: str) -> bytes:
+    """
+    taskes a string argument and encrypt it with bcrypt
+    steps:
+        encode the arg into a utf-8 string
+        then generates the salt of that argument with bcrypt.gensalt()
+        then hash or encrypt the paasword with bcrypt.hashpw method-
+        which takes two args the word to hash and the salt to hash it with
+    """
     password = password.encode("utf-8") #converts password to bytes
     salt = bcrpyt.gensalt() #creates salt to hash password with
     hashedpwd = bcrypt.hashpw(password, salt)
