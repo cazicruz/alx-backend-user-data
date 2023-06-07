a40fedd14ed54c8abb08cea3c2e166b88129951c
david onwuli <dcazicruz@icloud.com>
2023-06-07 18:58:10 +0100

    changed the __session to _session

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" a basic flask app"""
+from flask import Flask, jsonify
+
+
+app = Flask(__name__)
+app.config["SECRETE_KEY"] = "hardtoguess"
+
+@app.route("/", mathods=['GET'], strict_slashs=False)
+def get_payload():
+    payload = {"message": "Bienvenue"}
+    return jsonify(payload)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
